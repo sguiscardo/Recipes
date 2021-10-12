a0144ba2934c3fc97be40c40f3738d26a5e8b93c
Trevor Adcock <trevor.adcock@mac.com>
2021-10-12 15:21:22 -0700

    Initial Commit

---
+++ Recipe1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe1
+//
+//  Created by DevMountain on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

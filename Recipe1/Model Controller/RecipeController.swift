e120aac9a19ae383144e3db13375647b375006b6
Trevor Adcock <trevor.adcock@mac.com>
2021-10-12 16:35:10 -0700

    Reorders project files into organized groups and creates Storyboard views

---
+++ Recipe1/Model Controller/RecipeController.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeController.swift
+//  Recipe1
+//
+//  Created by DevMountain on 10/12/21.
+//
+
+import Foundation
+
+class RecipeController {
+    
+    static let shared = RecipeController()
+    
+    func createNewRecipe() {
+        
+    }
+    
+    
+}

6aaeda5390f6bd788b2403afa853bb1ee3b773ab
Trevor Adcock <trevor.adcock@mac.com>
2021-10-14 10:38:25 -0700

    Adds Filtering functionality for favorite recipes

--- Recipes/ViewControllers/RecipeTableViewController.swift
+++ Recipes/ViewControllers/RecipeTableViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 class RecipeTableViewController: UITableViewController {
     
     @IBOutlet weak var categoryNameTextField: UITextField!
+    @IBOutlet weak var filterSegmentedControl: UISegmentedControl!
     
     let recipeController = RecipeController.shared
     var category: RecipeCategory?
+    
+    private var filteredRecipes: [Recipe] {
+        if filterSegmentedControl.selectedSegmentIndex == 1 {
+            return category?.recipes.filter { $0.isFavorite } ?? []
+        } else {
+            return category?.recipes ?? []
+        }
+    }
 
     // MARK: - Lifecycle Methods
     override func viewWillAppear(_ animated: Bool) {
@@ -31,12 +40,12 @@ class RecipeTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return category?.recipes.count ?? 0
+        return filteredRecipes.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? RecipeTableViewCell,
-              let recipe = category?.recipes[indexPath.row] else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? RecipeTableViewCell else { return UITableViewCell() }
+        let recipe = filteredRecipes[indexPath.row]
         cell.recipe = recipe
         cell.delegate = self
         return cell
@@ -55,19 +64,26 @@ class RecipeTableViewController: UITableViewController {
         guard segue.identifier == "toRecipeDetail",
               let recipeDetailViewController = segue.destination as? RecipeDetailViewController,
               let selectedRow = tableView.indexPathForSelectedRow?.row else { return }
-        let recipe = category?.recipes[selectedRow]
+        let recipe = filteredRecipes[selectedRow]
         recipeDetailViewController.recipe = recipe
     }
     
     // MARK: - Actions
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
         guard let category = category else { return }
+        let oldRecipeCount = filteredRecipes.count
         recipeController.createRecipe(in: category)
-        let newRow = category.recipes.count - 1
+        let newRecipeCount = filteredRecipes.count
+        
+        guard newRecipeCount > oldRecipeCount else { return }
+        let newRow = filteredRecipes.count - 1
         let indexPath = IndexPath(row: newRow, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
     }
     
+    @IBAction func filterSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        tableView.reloadData()
+    }
 }
 
 // MARK: RecipeTableViewCellDelegate Conformance
@@ -76,6 +92,6 @@ extension RecipeTableViewController: RecipeTableViewCellDelegate {
     func toggleFavoriteButtonWasTapped(cell: RecipeTableViewCell) {
         guard let recipe = cell.recipe else { return }
         recipeController.toggleFavorite(recipe: recipe)
-        cell.updateViews()
+        tableView.reloadData()
     }
 }
